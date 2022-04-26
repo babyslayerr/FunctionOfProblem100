e929ea3297deb0650b249e7c1de7499105b8c7cf
JJH <gksqlcxkdns1@naver.com>
2022-04-26 20:49:00 +0900

    Initial commit

---
+++ src/main/java/com/example/functionofproblem100/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.example.functionofproblem100;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(FunctionOfProblem100Application.class);
+    }
+
+}
---
+++ src/test/java/com/example/functionofproblem100/FunctionOfProblem100ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.functionofproblem100;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FunctionOfProblem100ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
