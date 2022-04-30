4c94e6347087e33cfe28fe429a7c5837b7221dff
Bill <gksqlcxkdns1@naver.com>
2022-04-30 15:25:15 +0900

    2022-04-30

--- src/main/java/com/example/functionofproblem100/notice/controller/ApiController.java
+++ src/main/java/com/example/functionofproblem100/notice/controller/ApiController.java
@@ -3,28 +3,28 @@ package com.example.functionofproblem100.notice.controller;
 
 import com.example.functionofproblem100.notice.model.NoticeModel;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class ApiController {
 
 
-    @GetMapping("/api/notice")
-    public NoticeModel notice(){
-
-        LocalDateTime regDt = LocalDateTime.of(2021,1,30,0,0);
-
-        NoticeModel noticeModel = new NoticeModel();
-        noticeModel.setId(1);
-        noticeModel.setTitle("공지사항입니다.");
-        noticeModel.setContents("공지사항 내용입니다. ");
-        noticeModel.setRegDate(regDt);
-        return noticeModel;
-    }
+//    @GetMapping("/api/notice")
+//    public NoticeModel notice(){
+//
+//        LocalDateTime regDt = LocalDateTime.of(2021,1,30,0,0);
+//
+//        NoticeModel noticeModel = new NoticeModel();
+//        noticeModel.setId(1);
+//        noticeModel.setTitle("공지사항입니다.");
+//        noticeModel.setContents("공지사항 내용입니다. ");
+//        noticeModel.setRegDate(regDt);
+//        return noticeModel;
+//    }
 
     @GetMapping("/api/notice2")
     public NoticeModel apiNotice2(){
@@ -38,5 +38,14 @@ public class ApiController {
         noticeModel.setRegDate(regDate);
 
         return noticeModel;
+    }
+
+    @GetMapping("/api/notice")
+    public List<NoticeModel> notice(){
+
+        List<NoticeModel> noticeModelList = new ArrayList<>();
+
+        return noticeModelList;
+
     }
 }
--- src/main/java/com/example/functionofproblem100/notice/model/NoticeModel.java
+++ src/main/java/com/example/functionofproblem100/notice/model/NoticeModel.java
@@ -1,9 +1,14 @@
 package com.example.functionofproblem100.notice.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
-
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 @Data
 public class NoticeModel {
 
