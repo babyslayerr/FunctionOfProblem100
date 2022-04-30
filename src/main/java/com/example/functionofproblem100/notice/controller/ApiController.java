d729f35973ee91dfb83123f97c819f52d77fcf5d
Bill <gksqlcxkdns1@naver.com>
2022-04-30 17:35:19 +0900

    2022-04-30

--- src/main/java/com/example/functionofproblem100/notice/controller/ApiController.java
+++ src/main/java/com/example/functionofproblem100/notice/controller/ApiController.java
@@ -2,8 +2,7 @@ package com.example.functionofproblem100.notice.controller;
 
 
 import com.example.functionofproblem100.notice.model.NoticeModel;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -13,32 +12,42 @@ import java.util.List;
 public class ApiController {
 
 
-//    @GetMapping("/api/notice")
-//    public NoticeModel notice(){
+//    @GetMapping("/api/notice3")
+//    public List<NoticeModel> notice3(){
 //
-//        LocalDateTime regDt = LocalDateTime.of(2021,1,30,0,0);
+//        LocalDateTime regDt1 = LocalDateTime.of(2021,1,30,0,0);
+//        LocalDateTime regDt2 = LocalDateTime.of(2021,1,31,0,0);
+//
+//        List<NoticeModel> noticeModelList = new ArrayList<>();
+//        NoticeModel noticeModel1 = new NoticeModel();
+//        noticeModel1.setId(1);
+//        noticeModel1.setTitle("공지사항 입니다.");
+//        noticeModel1.setContents("공지사항 내용입니다. ");
+//        noticeModel1.setRegDate(regDt1);
+//        noticeModelList.add(noticeModel1);
+//
+//        NoticeModel noticeModel2 = new NoticeModel();
+//        noticeModel1.setId(2);
+//        noticeModel1.setTitle("두번째 공지사항 입니다.");
+//        noticeModel1.setContents("두번째 공지사항 내용입니다. ");
+//        noticeModel1.setRegDate(regDt2);
+//        noticeModelList.add(noticeModel2);
+//        return noticeModelList;
+//    }
+//
+//    @GetMapping("/api/notice2")
+//    public NoticeModel apiNotice2(){
+//
+//        LocalDateTime regDate = LocalDateTime.of(2021,2,8,0,0);
 //
 //        NoticeModel noticeModel = new NoticeModel();
 //        noticeModel.setId(1);
 //        noticeModel.setTitle("공지사항입니다.");
-//        noticeModel.setContents("공지사항 내용입니다. ");
-//        noticeModel.setRegDate(regDt);
+//        noticeModel.setContents("공지사항 내용입니다.");
+//        noticeModel.setRegDate(regDate);
+//
 //        return noticeModel;
 //    }
-
-    @GetMapping("/api/notice2")
-    public NoticeModel apiNotice2(){
-
-        LocalDateTime regDate = LocalDateTime.of(2021,2,8,0,0);
-
-        NoticeModel noticeModel = new NoticeModel();
-        noticeModel.setId(1);
-        noticeModel.setTitle("공지사항입니다.");
-        noticeModel.setContents("공지사항 내용입니다.");
-        noticeModel.setRegDate(regDate);
-
-        return noticeModel;
-    }
 
     @GetMapping("/api/notice")
     public List<NoticeModel> notice(){
@@ -47,5 +56,41 @@ public class ApiController {
 
         return noticeModelList;
 
+    }
+//
+//    @GetMapping("/api/notice/count")
+//    public int noticeCount(){
+//
+//        return 20;
+//    }
+    @PostMapping("/api/notice")
+    public NoticeModel addNotice(@RequestParam String title, @RequestParam String contents){
+
+        NoticeModel noticeModel = NoticeModel.builder()
+                .id(1)
+                .title(title)
+                .contents(contents)
+                .regDate(LocalDateTime.now())
+                .build();
+
+
+        return noticeModel;
+    }
+    @PostMapping("/api/notice2")
+    public NoticeModel addNotice2(NoticeModel noticeModel){
+
+        noticeModel.setId(2);
+        noticeModel.setRegDate(LocalDateTime.now());
+
+        return noticeModel;
+    }
+
+
+    @PostMapping("/api/notice3")
+    public NoticeModel addNotice3(@RequestBody NoticeModel noticeModel){  //json파일을 받기 위함
+        noticeModel.setId(3);
+        noticeModel.setRegDate(LocalDateTime.now());
+
+        return noticeModel;
     }
 }
